7986d54eff1c4323f52613f933b0723e6b8a055d
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-06-05 11:43:15 +0000

    Deploying to gh-pages from @ DanyilUA/goit-js-hw-09@5b23f6a2eb474721e3920cb0ade373a78f29dc12 🚀

---
+++ 01-color-switcher.754e8aa1.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),window:document.body};let n=null,e=!1;t.startBtn.addEventListener("click",(function(o){if(e)return;e=!0,n=setInterval((()=>{t.window.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(t){e=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.754e8aa1.js.map
